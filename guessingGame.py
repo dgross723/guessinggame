ddeee2479c81ef17a6a15733d47242e94b4a6146
Gross <379876@mcpsmd.org>
2019-11-20 09:06:54 -0500

    right, too hgih, and too low messages

--- guessingGame.py
+++ guessingGame.py
@@ -3,3 +3,15 @@ import random
 num = random.randrange(1,20)
 
 guess = input("Guess a number that is between 1 and 20")
+
+while guess !=
+
+    if guess > num
+
+        print ("YOU'RE WRONG, TOO HIGH")
+    
+    elif guess < num
+
+        print ("YOU'RE WRONG, TOO LOW")
+
+print ("You're right, good job I guess")
