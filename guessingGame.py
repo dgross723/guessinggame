442a20345aaa44c66dbd8c8bf2982a323a76dafa
Gross <379876@mcpsmd.org>
2019-11-21 08:41:27 -0500

    finished game

--- guessingGame.py
+++ guessingGame.py
@@ -1,17 +1,18 @@
 import random
 
 num = random.randrange(1,20)
+print(num)
 
-guess = input("Guess a number that is between 1 and 20")
+guess = int(input("Guess a number that is between 1 and 20 "))
 
-while guess !=
+while guess != num:
 
-    if guess > num
+    if guess > num:
 
-        print ("YOU'RE WRONG, TOO HIGH")
+       guess =  (int(input("YOU'RE WRONG, TOO HIGH, guess again ")))
     
-    elif guess < num
+    elif guess < num:
 
-        print ("YOU'RE WRONG, TOO LOW")
+        guess = (int(input("YOU'RE WRONG, TOO LOW, guess again ")))
 
-print ("You're right, good job I guess")
+print("You're right, good job I guess")
